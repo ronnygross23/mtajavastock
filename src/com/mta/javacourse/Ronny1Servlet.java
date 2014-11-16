5fe5b20487d4c60f4ed0d6e4d54e95032e52de47
ronny gross <ronnygross23@gmail.com>
2014-11-16 13:26:07 +0200

    TEST

--- src/com/mta/javacourse/Ronny1Servlet.java
+++ src/com/mta/javacourse/Ronny1Servlet.java
@@ -6,7 +6,7 @@ import javax.servlet.http.*;
 public class Ronny1Servlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		resp.setContentType("text/html");
-		int Radius=50;
+		int Radius=50,num1=0;
 		double Area,angleB=30,Hypotenuse=50,Opposite=0,Pow=0;
 		Area= Math.pow(Radius, 2)*Math.PI;
 		String line1 =new String("calculation 1:Area of circle with radius is:"+Area+"square-cm.");
