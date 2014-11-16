9b8bbd9e780c3cf5b6e005d4e20545d863fbb4e2
ronny gross <ronnygross23@gmail.com>
2014-11-16 13:21:47 +0200

    תרגיל חשבוני שעובד

--- src/com/mta/javacourse/Ronny1Servlet.java
+++ src/com/mta/javacourse/Ronny1Servlet.java
@@ -6,13 +6,16 @@ import javax.servlet.http.*;
 public class Ronny1Servlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		resp.setContentType("text/html");
-		int num1,num2,num3,num4=0;
-		num1=3;
-		num2=4;
-		num3=7;
-		num4=(num1+num2)*num3;
-		String resultstr = new String ("<h1>the result of"+"("+num1 +"+" +num2 +")" +"*" +num3 +"=" +num4+"</h1>");
-		
-		resp.getWriter().println(resultstr);
+		int Radius=50;
+		double Area,angleB=30,Hypotenuse=50,Opposite=0,Pow=0;
+		Area= Math.pow(Radius, 2)*Math.PI;
+		String line1 =new String("calculation 1:Area of circle with radius is:"+Area+"square-cm.");
+		angleB=(30*Math.PI)/180;
+		Opposite=Math.sin(angleB)*Hypotenuse;
+		String line2 =new String("calculation 2:Length of Opposite where angle B is 30 degrees and Hypotenuse length is 50 cm is:"+Opposite+"cm.");
+		Pow=Math.pow(20,13);
+		String line3 =new String("calculation 3: Power of 20 with exp of 13 is"+Pow);
+		String resultStr=new String (line1+"<br>"+line2+"<br>"+line3);
+		resp.getWriter().println(resultStr);
 	}
 }
